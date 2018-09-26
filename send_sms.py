0f610c788ecc5841a087080d96f384ff1b725cc3
DK <i386.net@gmail.com>
2018-09-26 08:27:07 +0300

    check api

--- send_sms.py
+++ send_sms.py
@@ -7,4 +7,10 @@ from smsc_api import *
 
 smsc = SMSC()
 
-smsc.send_sms('num', 'test', sender='sms')
+#s = smsc.send_sms('7926xxx', 'test', id=12346, sender='sms')
+#print(s)
+status = smsc.get_status(12346, '7926xxx', all=1)
+print(status)
+"""
+['1', '1537938113', '0', '1537938112', '7926xxx', '2.20', 'SMSC.RU~sms', 'Доставлено', 'test,0']
+"""
