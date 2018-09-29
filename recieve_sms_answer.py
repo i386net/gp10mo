bb0cd904255d40841f8075eb6ba248e61a4d1b1d
DK <i386.net@gmail.com>
2018-09-29 14:59:47 +0300

    added recieve sms answer

--- recieve_sms_answer.py
+++ recieve_sms_answer.py
@@ -3,6 +3,22 @@
 #
 
 import requests
-from smsc_api import *
-from service_access import *
+import service_access as sa
 
+
+def get_sms_answer():
+    url = 'https://smsc.ru/sys/get.php?get_answers=1'
+    # &login=<login>&psw=<password>
+    # login = sa.login
+    # password = sa.password
+    params = {'login': sa.login,
+              'psw': sa.password,
+              'fmt': '3',
+
+
+    }
+    r = requests.get(url, params)
+    print(r.text)
+
+
+get_sms_answer()
