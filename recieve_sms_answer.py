5312019cb474aba878b1acb93365d977bc82a781
dk_imlight <dk@i386.me>
2018-10-01 16:08:43 +0300

    added function to get max sms id

--- recieve_sms_answer.py
+++ recieve_sms_answer.py
@@ -7,15 +7,26 @@ import service_access as sa
 import sqlite3 as s3
 
 
+def max_sms_id():
+    """
+    Ищем максимальный ID смс сообщения
+    :return:
+    """
+    con = s3.connect('./project_files/gp10mo.db')
+    cursor = con.cursor()
+    query = 'SELECT MAX(sms_id) FROM answers'
+    get_max_sms_id = cursor.execute(query).fetchone()[0]
+    con.close()
+    # print(get_max_sms_id)
+    return get_max_sms_id
+
+
 def get_sms_answer():
     url = 'https://smsc.ru/sys/get.php?get_answers=1'
-    # &login=<login>&psw=<password>
-    # login = sa.login
-    # password = sa.password
     params = {'login': sa.login,
               'psw': sa.password,
               'fmt': '3',  # формат получения ответа в виде списка словарей"
-              'hour': '3'
+              'after_id': max_sms_id()  # макс ID смс сообщения
               }
     r = requests.get(url, params)
 
@@ -32,31 +43,18 @@ def update_db_with_answer(answer):
         con = s3.connect('./project_files/gp10mo.db')
         cursor = con.cursor()
 
-        insert_query = 'INSERT INTO answers(sms_id, received, phone_number, message, to_phone, sent) VALUES(?,?,?,?,?,?)'
+        insert_query = 'INSERT INTO ' \
+                       'answers(sms_id, received, phone_number, message, to_phone, sent) ' \
+                       'VALUES(?,?,?,?,?,?)'
         try:
             for element in answer:
                 element = tuple(element.values())
                 cursor.execute(insert_query, element)
                 con.commit()
-                con.close()
         except s3.IntegrityError as err:
             print(err)
+        con.close()
 
 
-
-
-
-    """
-    [{
-        "id": 47436134,
-        "received": "29.09.2018 15:07:39",
-        "phone": "79268401046",
-        "message": "1",
-        "to_phone": "79037676877",
-        "sent": "29.09.2018 15:07:37"
-        }]
-    """
-
-
-ans = get_sms_answer()
-update_db_with_answer(ans)
+#ans = get_sms_answer()
+#update_db_with_answer(ans)
