16ca2df07de4cdef32ffcfc4cafc3550aa3f5e16
DK <i386.net@gmail.com>
2018-10-01 11:16:32 +0300

    receive send sms

--- recieve_sms_answer.py
+++ recieve_sms_answer.py
@@ -4,6 +4,7 @@
 
 import requests
 import service_access as sa
+import sqlite3 as s3
 
 
 def get_sms_answer():
@@ -13,12 +14,33 @@ def get_sms_answer():
     # password = sa.password
     params = {'login': sa.login,
               'psw': sa.password,
-              'fmt': '3',
-
-
-    }
+              'fmt': '3',  # формат получения ответа в виде списка словарей"
+              'hour': '10'
+              }
     r = requests.get(url, params)
-    print(r.text)
+
+    return r.json()
 
 
-get_sms_answer()
+def update_db_with_answer(answer):
+    answer = answer[0]
+    con = s3.connect('./project_files/gp10mo.db')
+    cursor = con.cursor()
+
+
+
+
+    """
+    [{
+        "id": 47436134,
+        "received": "29.09.2018 15:07:39",
+        "phone": "79268401046",
+        "message": "1",
+        "to_phone": "79037676877",
+        "sent": "29.09.2018 15:07:37"
+        }]
+    """
+
+
+ans = get_sms_answer()
+update_db_with_answer(ans)
